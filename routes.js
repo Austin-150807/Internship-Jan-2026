cc9c910a4eb99678adb704236a07392a36b1ca6d
Austin <austin.150807@gmail.com>
2026-02-02 10:45:54 +0530

    edit and delete student

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 import createStudent from "./src/controllers/manageStudent/createStudent.js";
+import deleteStudent from "./src/controllers/manageStudent/deleteStudent.js";
+import editStudent from "./src/controllers/manageStudent/editStudent.js";
 import listStudent from "./src/controllers/manageStudent/listStudent.js";
 
 const router = (app) => {
   app.use("/api/create-student", createStudent);
   app.use("/api/list-student", listStudent);
+  app.use("/api/edit-student", editStudent);
+  app.use("/api/delete-student", deleteStudent);
 };
-
 export default router;
